5d8fffc5ab9879ae86238d1d14b5af514bdd9141
abusaddique95 <abusaddique95@live.com>
2022-09-16 18:26:28 +0100

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://<abusaddique95>:<P03HBp6VKaIfobPP>@cluster0.wk8zlyg.mongodb.net/?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
